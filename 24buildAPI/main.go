d06138fe097f2e36f4048323185403e0bc8d13b7
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-23 12:11:20 +0530

    Building API && sending a API json reponse

---
+++ 24buildAPI/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// model for course
+
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fake DB
+
+var courses []Course
+
+// middleware/helper file
+
+func (c *Course) isEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
+func main() {
+
+}
+
+// controller
+
+// serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome To API by Himanshu Chhatwal</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get All Courses")
+	w.Header().Set("Contnet-Type", "application/json")
+}
