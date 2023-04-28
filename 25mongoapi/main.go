910b9f73e37a24d9add23c0aa503917e5078fee7
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-28 23:01:46 +0530

    learned about connection in MongoDB

--- 25mongoapi/main.go
+++ 25mongoapi/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
 
-func main() {
-	fmt.Println("Working on backend module")
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionString = "mongodb+srv://himanshuchhatwal9295:himanshu2314@golang.9spcknm.mongodb.net/test"
+
+const dbName = "netflix"
+const colName = "watchlist"
+
+var collection *mongo.Collection
+
+//connect with mongo db
+
+func init() {
+	//client options
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("MongoDB connection success")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	fmt.Println("Collection reffrece is ready")
 }
